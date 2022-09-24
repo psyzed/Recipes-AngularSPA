5dd0f16f1eb4e27744b25aa0846069ec112d886f
Lazaros Poumpouridis <psyzed66@gmail.com>
2022-09-24 14:47:43 +0300

    selecting and populating the inputs with the selected ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
@@ -12,16 +12,24 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
   constructor(private shoppingListService: ShoppingListService) {}
+  @ViewChild('shoppingListForm') shoppingListForm: NgForm;
 
   startedEditingSubscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
 
   ngOnInit(): void {
     this.startedEditingSubscription =
       this.shoppingListService.startedEditing.subscribe((index: number) => {
         this.editedItemIndex = index;
         this.editMode = true;
+        this.editedItem = this.shoppingListService.getIngredient(index);
+        this.shoppingListForm.setValue({
+          //populating the inputs with the selected ingredient
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        });
       });
   }
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -11,15 +11,23 @@ export class ShoppingListService {
   ];
 
   getIngredients() {
+    //getting a copy of the ingredients array
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    //getting the ingredient that we want to select and edit from the array
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient) {
+    //adding a new ingredient and nexting that the ingredients array has changed
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
+    //addind multiple ingredients and nexting that the ingredients array has changed!
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
