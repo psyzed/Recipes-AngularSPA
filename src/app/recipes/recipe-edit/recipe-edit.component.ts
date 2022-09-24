ded158625a58170ca92835cb87fe7fd918559507
Lazaros Poumpouridis <psyzed66@gmail.com>
2022-09-24 18:13:57 +0300

    added the ingredient inputs array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -27,22 +28,41 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let imgPath = '';
     let recipeDescription = '';
+    let recipeIngredientsFormArray = new FormArray([]); //the formArray is initialized as an empty array
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipeById(this.recipeId);
       recipeName = recipe.name;
       imgPath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredientsFormArray.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName, Validators.required),
       imgPath: new FormControl(imgPath, Validators.required),
       description: new FormControl(recipeDescription, Validators.required),
+      ingredientsInputArray: recipeIngredientsFormArray,
     });
+  }
+  get ingredientControls() {
+    return (<FormArray>this.recipeForm.get('ingredientInputArray') as FormArray).controls;
   }
 }
