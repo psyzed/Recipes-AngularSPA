49b07dd2f973ea30b6564faa1bb1409389f55ab1
Lazaros Poumpouridis <psyzed66@gmail.com>
2022-09-24 17:04:40 +0300

    initielized the recipe form and conected it to the template

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -12,13 +13,36 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.recipeId = +params['id']; //the string 'id' is added cause this is the name we chose for the dynamic parameter in our route set up
       this.editMode = params['id'] != null; //checking if we are in edit mode
+      this.initForm();
       // console.log(this.editMode);
+    });
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let imgPath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipeById(this.recipeId);
+      recipeName = recipe.name;
+      imgPath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName, Validators.required),
+      imgPath: new FormControl(imgPath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
     });
   }
 }
