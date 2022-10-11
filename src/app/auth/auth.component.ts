ee78b08f60ec20f4952eaf03d2173e593d74f173
lazaros <psyzed66@gmail.com>
2022-10-11 16:25:45 +0300

    added and implemented authentication component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+})
+export class AuthComponent {
+  isLoginMode = true;
+
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    if (!authForm.valid) {
+      return;
+    }
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    if (this.isLoginMode) {
+      //login...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
+    authForm.reset();
+  }
+}
