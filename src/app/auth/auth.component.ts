8843881b7c3b3f949d60ab605f9da5d7d252bb06
lazaros <psyzed66@gmail.com>
2022-10-12 12:54:48 +0300

    Implemented authentication requests and created a user model, also implemented token expiration date functionality

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,6 +10,7 @@ import { AuthService } from './auth.service';
 export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {}
 
@@ -23,21 +25,26 @@ export class AuthComponent {
     const email = authForm.value.email;
     const password = authForm.value.password;
 
+    let authObservable: Observable<AuthResponseData>;
+
     this.isLoading = true;
     if (this.isLoginMode) {
-      //login...
+      authObservable = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (error) => {
-          console.log(error);
-          this.isLoading = false;
-        }
-      );
+      authObservable = this.authService.signup(email, password);
     }
+
+    authObservable.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
 
     authForm.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,23 +1,98 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Subject, tap, throwError } from 'rxjs';
+import { User } from './user.model';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCFmkWQPj9TobhrOn-LllCtovjAPoGTpIY',
-      { email: email, password: password, returnSecureToken: true }
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCFmkWQPj9TobhrOn-LllCtovjAPoGTpIY',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCFmkWQPj9TobhrOn-LllCtovjAPoGTpIY',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const tokenExpirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000
     );
+    const user = new User(email, userId, token, tokenExpirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An Unknown Error Occured!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This Email Exists Already!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = `This Email Doesn't Exist!`;
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Wrong Credentials!';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'This User Is Disabled!';
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password Sign-In is Disabled for this Project!';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Too Many Failed Attempts. Try Again Later!';
+    }
+    return throwError(errorMessage);
   }
 }
